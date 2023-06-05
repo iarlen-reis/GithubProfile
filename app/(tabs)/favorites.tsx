c8db8341fef7a94c496a0febf710f41850af745b
Iarlen <iarlensr@hotmail.com>
2023-06-05 17:18:56 -0300

    feat: creating route of favorites.

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const favorites = () => {
   return (
-    <View>
-      <Text>favorites</Text>
+    <View className="flex-1 bg-secundary">
+      <Text className="font-title text-white">favorites</Text>
     </View>
   )
 }
