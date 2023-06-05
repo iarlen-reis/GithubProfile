51d11356ca51237e30491afb18d62ed55b78055c
Iarlen <iarlensr@hotmail.com>
2023-06-05 17:18:27 -0300

    feat: implementing Search functionality in the Search component.

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TextInput, TouchableOpacity } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 
+import { useProfileContext } from '../contexts/ProfileContext'
+
 const Search = () => {
+  const [search, setSearch] = useState<string>('')
+  const { getProfile } = useProfileContext()
+
+  const handleProfile = () => {
+    if (!search) return
+
+    getProfile(search.toLowerCase())
+  }
   return (
     <View className="relative mt-5 w-full flex-row items-center">
       <Feather
@@ -15,8 +25,12 @@ const Search = () => {
         placeholder="Search Github username..."
         className="w-full rounded-2xl bg-primary px-[45] py-[18] font-body text-[12px] text-white"
         placeholderTextColor="#FFF"
+        onChangeText={(text) => setSearch(text)}
       />
-      <TouchableOpacity className="absolute right-2 rounded-xl bg-link px-4 py-3">
+      <TouchableOpacity
+        className="absolute right-2 rounded-xl bg-link px-4 py-3"
+        onPress={handleProfile}
+      >
         <Text className="font-title text-sm text-white">Search</Text>
       </TouchableOpacity>
     </View>
