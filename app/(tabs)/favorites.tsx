b61d9ef73b700e3c14d0d937487f923382fa74c4
Iarlen <iarlensr@hotmail.com>
2023-06-05 21:37:30 -0300

    feat: creating page of Favorites page and adding styles.

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -1,12 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React from 'react'
+import ProfileCard from '../../src/components/ProfileCard'
+import { useFavoriteContext } from '../../src/contexts/FavoriteContext'
 
-const favorites = () => {
+const Favorites = () => {
+  const { favorites } = useFavoriteContext()
+
+  console.log(favorites)
+
   return (
     <View className="flex-1 bg-secundary">
-      <Text className="font-title text-white">favorites</Text>
+      <Text className="mt-5 font-title text-lg text-white">
+        Seus Perfis favoritos
+      </Text>
+      <FlatList
+        data={favorites}
+        keyExtractor={(item) => item.login}
+        renderItem={({ item }) => (
+          <ProfileCard profile={item} key={item.login} />
+        )}
+      />
     </View>
   )
 }
 
-export default favorites
+export default Favorites
