1188b701a1ad2a71748d1e0d19e84601807cde66
Iarlen <iarlensr@hotmail.com>
2023-06-05 23:09:27 -0300

    refactor: adding conditional for if there is no profile added to favorites.

--- app/(tabs)/favorites.tsx
+++ app/(tabs)/favorites.tsx
@@ -2,6 +2,7 @@ import { View, Text, FlatList } from 'react-native'
 import React from 'react'
 import ProfileCard from '../../src/components/ProfileCard'
 import { useFavoriteContext } from '../../src/contexts/FavoriteContext'
+import { Fontisto } from '@expo/vector-icons'
 
 const Favorites = () => {
   const { favorites } = useFavoriteContext()
@@ -11,14 +12,28 @@ const Favorites = () => {
       <Text className="my-5 font-title text-lg text-white">
         Seus Perfis favoritos
       </Text>
-      <FlatList
-        data={favorites}
-        keyExtractor={(item) => item.login}
-        renderItem={({ item }) => (
-          <ProfileCard profile={item} key={item.login} />
-        )}
-        showsVerticalScrollIndicator={false}
-      />
+      {favorites.length > 0 ? (
+        <FlatList
+          data={favorites}
+          keyExtractor={(item) => item.login}
+          renderItem={({ item }) => (
+            <ProfileCard profile={item} key={item.login} />
+          )}
+          showsVerticalScrollIndicator={false}
+        />
+      ) : (
+        <View className="items-center justify-center">
+          <Fontisto
+            name="favorite"
+            size={45}
+            color="#8E0000"
+            style={{ marginTop: '50%' }}
+          />
+          <Text className="mt-5 text-center font-title text-base text-white">
+            Nenhum perfil adicionado aos favoritos.
+          </Text>
+        </View>
+      )}
     </View>
   )
 }
