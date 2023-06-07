61367878a7c9b704e5e48478dbade33d47cb0aba
Iarlen <iarlensr@hotmail.com>
2023-06-07 18:32:17 -0300

    refactor: adding the queryClientProvider in the layout.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,43 +4,49 @@ import { Feather } from '@expo/vector-icons'
 import { FavoriteProvider } from '../../src/contexts/FavoriteContext'
 import { ProfileProvider } from '../../src/contexts/ProfileContext'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
 const TabLayout = () => {
+  const queryClient = new QueryClient()
+
   return (
     <View className="flex-1 bg-secundary">
-      <ProfileProvider>
-        <FavoriteProvider>
-          <Tabs
-            screenOptions={{
-              headerShown: false,
-              tabBarActiveBackgroundColor: 'transparent',
-              tabBarItemStyle: { backgroundColor: '#141D2F' },
-              tabBarStyle: {
-                backgroundColor: 'transparent',
-                borderTopColor: '#141D2F',
-              },
-              tabBarShowLabel: false,
-            }}
-          >
-            <Tabs.Screen
-              name="home"
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Feather name="home" color={color} size={size} />
-                ),
+      <QueryClientProvider client={queryClient}>
+        <ProfileProvider>
+          <FavoriteProvider>
+            <Tabs
+              screenOptions={{
+                headerShown: false,
+                tabBarActiveBackgroundColor: 'transparent',
+                tabBarItemStyle: { backgroundColor: '#141D2F' },
+                tabBarStyle: {
+                  backgroundColor: 'transparent',
+                  borderTopColor: '#141D2F',
+                },
+                tabBarShowLabel: false,
               }}
-            />
-            <Tabs.Screen
-              name="favorites"
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <Feather name="heart" color={color} size={size} />
-                ),
-              }}
-            />
-            <Tabs.Screen name="repositories" options={{ href: null }} />
-          </Tabs>
-        </FavoriteProvider>
-      </ProfileProvider>
+            >
+              <Tabs.Screen
+                name="home"
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Feather name="home" color={color} size={size} />
+                  ),
+                }}
+              />
+              <Tabs.Screen
+                name="favorites"
+                options={{
+                  tabBarIcon: ({ color, size }) => (
+                    <Feather name="heart" color={color} size={size} />
+                  ),
+                }}
+              />
+              <Tabs.Screen name="repositories" options={{ href: null }} />
+            </Tabs>
+          </FavoriteProvider>
+        </ProfileProvider>
+      </QueryClientProvider>
     </View>
   )
 }
