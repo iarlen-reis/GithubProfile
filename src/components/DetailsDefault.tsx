4b86f75f271d184a64f0df00a7ce2a5c122d4c64
Iarlen <iarlensr@hotmail.com>
2023-06-08 15:55:56 -0300

    refactor: separating the initial default screen into a component called DetailsDefault.

---
+++ src/components/DetailsDefault.tsx
@@ -0,0 +1,21 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+import ProfileImage from '../assets/profileImage.png'
+
+const DetailsDefault = () => {
+  return (
+    <View className="items-center justify-center">
+      <Image
+        source={ProfileImage}
+        alt="profile image"
+        className="h-[70px] w-[70px] rounded-[35px]"
+      />
+      <Text className="mt-5 font-title text-white">
+        Pesquise por um usuário!
+      </Text>
+    </View>
+  )
+}
+
+export default DetailsDefault
