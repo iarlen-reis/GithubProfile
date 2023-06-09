a846eda0f2f623e3471cb354e4b423d4ecc72738
Iarlen <iarlensr@hotmail.com>
2023-06-08 22:45:30 -0300

    chore: configure yserInterfaceStyle.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx', './app/**/*.tsx'],
-  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
